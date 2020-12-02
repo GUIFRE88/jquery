84a177a4edd12f2d0363944658d7498a99c9de62
Guilherme F <guilherme.fre073@gmail.com>
2020-12-01 21:57:26 -0300

    Finalizado projeto

---
+++ public/js/placar.js
@@ -0,0 +1,33 @@
+function inserePlacar(){
+    var corpoTabela = $(".placar").find("tbody"); // Busca dentro da section
+    var usuario = "Guilherme";
+    var numPalavras = $("#contador-palavras").text(); // Busca o numero de palavras
+    var linha = novaLinha(usuario,numPalavras);
+
+    linha.find(".botao-remover").click(removeLinha); // Adiciona a função ao clicar no botão.
+
+    corpoTabela.append(linha); // Adiciona a linha no HTML
+}
+
+function novaLinha(usuario,numPalavras){
+
+    var linha = $("<tr>"); // Cria elemento em HTML por jquery
+    var colunaUsuario = $("<td>").text(usuario); 
+    var colunaPalavras = $("<td>").text(numPalavras); 
+    var colunaRemover = $("<td>"); 
+    var link = $("<a>").addClass("botao-remover").attr("href","#");
+    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
+
+    link.append(icone); // Adicionado o icone dentro do link
+    colunaRemover.append(link);
+    linha.append(colunaUsuario);
+    linha.append(colunaPalavras);
+    linha.append(colunaRemover);
+
+    return linha;
+}
+
+function removeLinha(event){
+    event.preventDefault(); // Retira comportamento padrão do link, de jogar para o inicio da página.
+    $(this).parent().parent().remove();
+}
