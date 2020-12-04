2ea35a6fa4f5b58668da04d652c659ad9c2591d1
Guilherme F <guilherme.fre073@gmail.com>
2020-12-04 13:22:14 -0300

    Ajax para acessar servidor

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,7 +1,18 @@
 $("#botao-frase").click(fraseAleatoria); // BUsca botão e chama função ao clicar
+$("#botao-frase-id").click(buscaFrase); // BUsca botão e chama função ao clicar
 
 function fraseAleatoria(){
-    $.get("http://localhost:3000/frases", trocaFraseAleatoria); // Faz a requisição AJAX
+    $("#spinner").show(); // Mostra icone de carregamento
+    $.get("http://localhost:3000/frases", trocaFraseAleatoria)
+    .fail(function(){
+        $("#erro").show(); // Mostra o erro na tela.
+        setTimeout(function(){
+            $("#erro").toggle() // Retira o erro da tela após 2 segundo
+        },2000)
+    }) // Faz a requisição AJAX, o fail é pra quando ocorre erro
+    .always(function(){ // Always, chama sempre, se deu erro ou se deu sucesso.
+        $("#spinner").toggle(); // Retira icone de carregamento
+    })
 }
 
 function trocaFraseAleatoria(data){
@@ -10,4 +21,28 @@ function trocaFraseAleatoria(data){
     frase.text(data[numeroAleatorio].texto); // Adiciona a frase na tela.
     atualizaTamanhoFrase();
     atualizaTempoInicial(data[numeroAleatorio].tempo);
+}
+function buscaFrase(){
+    $("#spinner").show(); // Mostra icone de carregamento
+    var fraseId = $("#frase-id").val(); // Busca valor do input
+    var dados = { id: fraseId} ; 
+    $.get("http://localhost:3000/frases",dados,trocaFrase)
+    .fail(function(){
+        $("#erro-id").show(); // Mostra o erro na tela.
+        setTimeout(function(){
+            $("#erro-id").toggle() // Retira o erro da tela após 2 segundo
+        },2000)
+    }) // Faz a requisição AJAX, o fail é pra quando ocorre erro
+    .always(function(){ // Always, chama sempre, se deu erro ou se deu sucesso.
+        $("#spinner").toggle(); // Retira icone de carregamento
+    })
+}
+
+function trocaFrase(data){
+    var frase = $(".frase"); // Busca a frase
+    frase.text(data.texto); // Adiciona a frase na tela.
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data.tempo);
+    
+
 }
